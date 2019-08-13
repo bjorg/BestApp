6164c984281286a43581008bda20ebac81352a68
Steve G. Bjorg <steve.bjorg@gmail.com>
2019-08-13 13:50:01 -0700

    first pass

---
+++ BestAppFunction/Function.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
+using LambdaSharp;
+using LambdaSharp.ApiGateway;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace My.BestApp.BestAppFunction {
+
+    public class PutRequest {
+
+        //--- Properties ---
+        [JsonRequired]
+        public string Name { get; set; }
+
+        [JsonRequired]
+        public string Content { get; set; }
+    }
+
+    public class PutResponse {
+
+        //--- Properties ---
+        public string Status { get; set; }
+    }
+
+    public class Function : ALambdaApiGatewayFunction {
+
+        //--- Fields ---
+        private string _bucketName;
+        private IAmazonS3 _s3Client;
+
+        //--- Methods ---
+        public override async Task InitializeAsync(LambdaConfig config) {
+            _bucketName = config.ReadS3BucketName("BestAppBucket");
+            _s3Client = new AmazonS3Client();
+        }
+
+        public async Task<PutResponse> Put(PutRequest request) {
+            await _s3Client.PutObjectAsync(new PutObjectRequest {
+                BucketName = _bucketName,
+                Key = request.Name,
+                ContentBody = request.Content
+            });
+            return new PutResponse {
+                Status = "success"
+            };
+        }
+    }
+}
